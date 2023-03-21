7227663883137b1f94c1792ce2f9225f2c2f807b
ReusHarper <reusharper@comunidad.unam.mx>
2023-03-20 21:42:46 -0600

    :wrench: [COMPONENT - TABBAR][FRONT-END] Creacion de tab bar con las 5 secciones principales de la app

---
+++ Proteco/Model/TabModel.swift
@@ -0,0 +1,20 @@
+//
+//  TabModel.swift
+//  Proteco
+//
+//  Created by ReusHarper on 20/03/23.
+//
+
+import SwiftUI
+
+struct TabModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabModel_Previews: PreviewProvider {
+    static var previews: some View {
+        TabModel()
+    }
+}
