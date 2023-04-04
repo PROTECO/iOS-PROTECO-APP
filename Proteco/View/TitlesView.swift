7c7cee06c818fc8c59d97195740f678f6b49e223
ReusHarper <reusharper@comunidad.unam.mx>
2023-04-04 11:54:44 -0600

    :sparkles: [COMPONENT - TITLES][FRONT-END] Generacion de titulos base para vistas

--- Proteco/View/TitlesView.swift
+++ Proteco/View/TitlesView.swift
@@ -1,20 +1,32 @@
-//
-//  TitlesView.swift
-//  Proteco
-//
-//  Created by ReusHarper on 03/04/23.
-//
-
+// Dependencies
 import SwiftUI
 
 struct TitlesView: View {
+    // MARK: - PROPERTIES
+    var title       : String
+    var description : String?
+    var fgColorTitle: Color?
+    var fgColorDesc : Color?
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            Text(title)
+                .font(.custom("MontserratAlternates-Bold" , size: 35))
+                .foregroundColor(fgColorTitle != nil ? fgColorTitle! : Color("color_dark"))
+            
+            Text(description != nil ? description! : "")
+                .font(.custom("Assistant-Regular" , size: 18))
+                .foregroundColor(fgColorDesc != nil ? fgColorDesc! : Color("color_dark"))
+        } //: VSTACK
     }
 }
 
+// MARK: - PREVIEW
 struct TitlesView_Previews: PreviewProvider {
     static var previews: some View {
-        TitlesView()
+        TitlesView(title: "Testing", description: "Hello world!", fgColorTitle: Color("color_main"))
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
